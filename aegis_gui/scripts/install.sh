dab3269c1ab20c01e9b18db6baa5639b81d33d87
D3vil0p3r <>
2023-09-24 22:53:42 +0200

    Some fix

--- aegis_gui/scripts/install.sh
+++ aegis_gui/scripts/install.sh
@@ -1,4 +1,2 @@
 #!/usr/bin/bash
-echo "Running reflector to sort for fastest mirrors"
-pkexec reflector --latest 5 --sort rate --save /etc/pacman.d/mirrorlist
 pkexec athena-aegis config ~/.config/aegis.json
